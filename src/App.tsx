3cf8c3160d7be9c532282d13cb12d3b780a7f4ca
ravi kumar dhawan <ravidhawan8899@gmail.com>
2023-03-16 23:13:20 +0530

    components allmovie and my watchlist created

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { Box, ChakraProvider, Container, Heading } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
-  return <></>;
+  const queryClient = new QueryClient();
+  return (
+    <>
+      <ChakraProvider>
+        <BrowserRouter>
+          <QueryClientProvider client={queryClient}>
+            <Heading> welcome to Movie web app</Heading>
+            {/* <Routes>
+              <Route path="/" />
+              <Route path="/" />
+              <Route path="/" />
+            </Routes> */}
+          </QueryClientProvider>
+        </BrowserRouter>
+      </ChakraProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/AllMovies.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AllMovies() {
+  return <div>all movies</div>;
+}
+
+export default AllMovies;
